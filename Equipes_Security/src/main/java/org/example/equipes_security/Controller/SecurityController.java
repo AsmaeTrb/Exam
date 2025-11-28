1abfd394602c3f19aa3e947f035c3c2983a57858
Asmae <asmae@gmail.com>
2025-11-28 13:33:35 +0100

    version final

--- Equipes_Security/src/main/java/org/example/equipes_security/Controller/SecurityController.java
+++ Equipes_Security/src/main/java/org/example/equipes_security/Controller/SecurityController.java
@@ -3,7 +3,11 @@ package org.example.equipes_security.Controller;
 import org.example.equipes_security.Service.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 
@@ -13,5 +17,10 @@ public class SecurityController {
     public SecurityController(PasswordEncoder passwordEncoder, UserDetails userDetails) {
         this.passwordEncoder = passwordEncoder;
         this.userDetails = userDetails;
+    }
+    @PostMapping("/login")
+    public Map<String,String> login(@RequestParam String username, @RequestParam String password) {
+
+
     }
 }
--- Equipes_Security/src/main/java/org/example/equipes_security/Service/UserDetails.java
+++ Equipes_Security/src/main/java/org/example/equipes_security/Service/UserDetails.java
@@ -1,14 +1,20 @@
 package org.example.equipes_security.Service;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 @Service
 public class UserDetails implements UserDetailsService {
+    private PasswordEncoder passwordEncoder;
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = User.withUsername("admin").password("1234").build();
+
+
+
     }
 }
